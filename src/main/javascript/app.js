1e7cd6bef050a40143ed9fa11cccb47b10d4a767
caravenase <caravena@nisum.com>
2015-08-31 18:17:09 -0300

    changed js directory by src/main/javascript

--- src/main/javascript/app.js
+++ src/main/javascript/app.js
@@ -1,22 +1,26 @@
 var main = function() {
   $('#submit').click(function(){
       var user = $('#user').val();
-      if(validateAlphanumericUser(user) && validateLengthUser(user)){
-        console.log("pass to the next page");
+      if(!validateAlphanumericUser(user) || !validateLengthUser(user)){
+        $('#login').submit(function(event){
+            event.preventDefault();
+        });
+      }else{
+        return;
       }
+
   });
-};
+}
 
 var validateAlphanumericUser = function(user){
     var regex = /^[a-zA-Z0-9][a-zA-Z0-9]+$/;
     if(!user.match(regex)){
       alert('You can use only numbers and letter');
       return false;
-
     }else {
         return true;
     }
-};
+}
 
 var validateLengthUser = function(user){
     if(user.length <= 4 || user.length >= 30) {
--- src/test/lib/spec_app.js
+++ src/test/lib/spec_app.js
@@ -1,19 +1,19 @@
-describe("Login Happy Path",function(){
+describe("Validated User",function(){
 	describe("Given a User",function(){
 		it("I want to validate username constraints",function(){
-			expect(validateLengthUser("caravena")).toBe(true);
+			expect(validateLengthUser("!#$%qwer")).toBe(true);
 		});
 	});
 });
-describe("Login with Validation errors",function(){
+describe("Short name",function(){
 	describe("Given a User",function(){
 		it("I want to validate username is shorter them 4",function(){
-			expect(validateLengthUser("abc")).toBe(false)
+			expect(validateLengthUser("abc")).toBe(false);
 		});
 	});
 });
 
-describe("Login with Validation errors",function(){
+describe("Long name",function(){
 	describe("Given a User",function(){
 		it("I want to validate username is longer them 30",function(){
 			expect(validateLengthUser("abc123qwe3abc123qwe3abc123qwe3")).toBe(false);
@@ -21,10 +21,18 @@ describe("Login with Validation errors",function(){
 	});
 });
 
-describe("Login with Validation errors",function(){
+describe("Wrong character in name",function(){
 	describe("Given a User",function(){
 		it("I want to validate alphanumeric username",function(){
 			expect(validateAlphanumericUser("!#$%qwer")).toBe(false)
+		});
+	});
+});
+
+describe("Empty name ",function(){
+	describe("Given a User",function(){
+		it("I want to validate alphanumeric username",function(){
+			expect(validateAlphanumericUser("")).toBe(false)
 		});
 	});
 });
